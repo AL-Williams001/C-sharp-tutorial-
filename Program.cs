3cbb4851ab3e3df6d2f0377da7ef04f4b324860b
AL-Williams001 <arthurlynnwilliams01@gmail.com>
2024-06-22 00:18:24 +0800

    learn basic of strings and searching for strings

--- Program.cs
+++ Program.cs
@@ -11,6 +11,15 @@ firstFriend = firstFriend.Trim();
 
 string secondFriend = "     Mary    ";
 
+
 // $ = string interpolation/concatination 
-Console.WriteLine($"My friends are {firstFriend} and {secondFriend.Trim()}!");
+string friends = $"My friends are {firstFriend} and {secondFriend.Trim()}!";
+
+Console.WriteLine(friends);
+friends = friends.Replace("Mary", "Scott");
+Console.WriteLine(friends);
+Console.WriteLine(friends.ToUpper());
+Console.WriteLine (friends.Contains("John"));
+Console.WriteLine(friends.Length);
+
 
