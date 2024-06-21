fb2309983f9aa141a6c0832745e2bd1672dec6c9
AL-Williams001 <arthurlynnwilliams01@gmail.com>
2024-06-21 23:32:07 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, AL!");
+
+
+string firstFriend = "    John    ";
+firstFriend = firstFriend.Trim();
+
+
+
+
+
+string secondFriend = "     Mary    ";
+
+// $ = string interpolation/concatination 
+Console.WriteLine($"My friends are {firstFriend} and {secondFriend.Trim()}!");
+
